2c79c3d70e81cea740c90af8437a3ceb719b6d40
Luiggyblanco24 <24luiggy@gmail.com>
2024-05-27 19:49:24 -0500

    completado de la parte estetica, falta contacto

--- js/jsindex.js
+++ js/jsindex.js
@@ -2,6 +2,14 @@ function load_page(){
     Swal.fire({
         title: "APROVECHA LAS PROMOCIONES EN BLANTEC",
         imageAlt: "A tall image",
-        showConfirmButton: true,
+        showConfirmButton: false,
+        timer:2000,
       });
 }
+
+//function entra(params) {
+  //document.getElementById("container").style.backgroundColor="#008080";
+//}
+//function sale(params) {
+  //document.getElementById("container").style.backgroundColor="#fff";
+//}
